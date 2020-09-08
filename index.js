2de236e2817815160fa6cd093b64c48acc833350
Awilliams4321 <awilliams1234@live.com>
2020-09-08 13:02:08 -0600

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(team) {
+  let win = team.find(team => team.result === "W")
+  if (win) {
+    return win.year
+  }
+  else {
+    return undefined
+  }
+}
